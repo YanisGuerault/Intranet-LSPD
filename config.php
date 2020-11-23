81ba8b3eed82696edf40223f98ef3b91382b6f59
Yanis <40592598+YanisGuerault@users.noreply.github.com>
2020-11-23 10:34:12 +0100

    Update config.php

--- config.php
+++ config.php
@@ -85,7 +85,7 @@ function get_rapport_all()
     return mysqli_fetch_array($resultat2);
 }
 
-$con = mysqli_connect ('192.168.0.25','yanis','585652yanis','LSPD_Osmoze_RP') or die ('Impossible de ce connecter');
+$con = mysqli_connect ('','','','LSPD_Osmoze_RP') or die ('Impossible de ce connecter');
 mysqli_set_charset($con, 'utf8');
 
 ?>
